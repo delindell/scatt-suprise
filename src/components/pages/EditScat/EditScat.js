ca58143e55d93e111178a4d2c1d13eeacb371e08
delindell <daviselindell@gmail.com>
2020-06-01 21:34:42 -0500

    routes done

--- src/components/pages/EditScat/EditScat.js
+++ src/components/pages/EditScat/EditScat.js
@@ -4,9 +4,11 @@ import './EditScat.scss';
 
 class EditScat extends React.Component {
   render() {
+    const { editId } = this.props.match.params.scatId;
     return (
       <div className="EditScat">
         <h1>EditScat</h1>
+        <h3>the scatId is {editId}</h3>
       </div>
     );
   }
