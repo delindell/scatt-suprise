fc3cc387ea61235d481b0de5c409cab64025400b
delindell <daviselindell@gmail.com>
2020-06-06 09:35:41 -0500

    wip

--- src/components/shared/MyNavBar/MyNavBar.js
+++ src/components/shared/MyNavBar/MyNavBar.js
@@ -1,20 +1,79 @@
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 
 import './MyNavBar.scss';
 
 class MyNavBar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   logoutEvent = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   }
 
+  // hooks for reactStrap
+  // const Example = (props) => {
+  //   const [isOpen, setIsOpen] = useState(false);
+  //   const toggle = () => setIsOpen(!isOpen);
+
   render() {
+    const { isOpen } = this.state;
     return (
       <div className="MyNavBar">
-        <h1>My NAV</h1>
-        <button className="btn btn-warning" onClick={this.logoutEvent}>Logout</button>
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+        {/* <button className="btn btn-warning" onClick={this.logoutEvent}>Logout</button> */}
       </div>
     );
   }
